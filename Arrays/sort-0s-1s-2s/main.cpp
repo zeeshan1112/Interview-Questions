c7fd892424b52d1d34a9638177746e8affe16783
Zeeshan Ahmad <zeeshan.official@gmail.com>
2016-04-16 16:20:07 +0530

    Removed comments from the code of sort-0s-1s-2s

--- Arrays/sort-0s-1s-2s/main.cpp
+++ Arrays/sort-0s-1s-2s/main.cpp
@@ -1,26 +1,3 @@
-/*Sort an array of 0s, 1s and 2s
-Given an array A[] consisting 0s, 1s and 2s, write a function that sorts A[]. The functions should put all 0s first, then all 1s and all 2s in last.
-
-Example
-Input = {0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1};
-Output = {0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2}
-
-ALGORITHM
-The problem was posed with three colours, here `0′, `1′ and `2′. The array is divided into four sections:
-
-a[1..Lo-1] zeroes (red)
-a[Lo..Mid-] ones (white)
-a[Mid..Hi] unknown
-a[Hi+1..N] twos (blue)
-The unknown region is shrunk while maintaining these conditions
-
-1. Lo := 1; Mid := 1; Hi := N;
-2. while Mid <= Hi do
-    1. Invariant: a[1..Lo-1]=0 and a[Lo..Mid-1]=1 and a[Hi+1..N]=2; a[Mid..Hi] are unknown.
-    2. case a[Mid] in
-        0: swap a[Lo] and a[Mid]; Lo++; Mid++
-        1: Mid++
-        2: swap a[Mid] and a[Hi]; Hi--*/
 
 #include <bits/stdc++.h>
 
