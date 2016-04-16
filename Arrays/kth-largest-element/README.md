a59f39fbaeb7a47f766878fe6259a8ef4fea0631
Zeeshan Ahmad <zeeshan.official@gmail.com>
2016-04-16 15:28:29 +0530

    Added README for kth-largest-element

---
+++ Arrays/kth-largest-element/README.md
@@ -0,0 +1,9 @@
+### Kth largest(or smallest) elements in an array - [minHeap method]
+> ALGORITHM
+1. Build a Min Heap of the first k elements (arr[0] to arr[k-1]) of the given array. **O(k)**
+2. For each element, after the kth element (arr[k] to arr[n-1]), compare it with root of min heap.
+If the element is greater than the root then make it root and call heapify for min heap.
+else ignore it. **O((n-k) * logk)**
+3. Finally, min heap has k largest elements and root of the min heap is the kth largest element.
+
+> Time Complexity: **O(k + (n-k) * logk)** 
--- Arrays/kth-largest-element/main.cpp
+++ Arrays/kth-largest-element/main.cpp
@@ -1,17 +1,3 @@
-/*
-K th smallest element in an array
-
-ALGORITHM
-1) Build a Min Heap of the first k elements (arr[0] to arr[k-1]) of the given array. O(k)
-
-2) For each element, after the kth element (arr[k] to arr[n-1]), compare it with root of min heap.
-If the element is greater than the root then make it root and call heapify for min heap.
-else ignore it. The step 2 is O((n-k)*logk)
-
-3) Finally, min heap has k largest elements and root of the min heap is the kth largest element.
-
-Time Complexity: O(k + (n-k)Logk) without sorted output. If sorted output is needed then O(k + (n-k)Logk + kLogk)*/
-
 #include <bits/stdc++.h>
 using namespace std;
 
